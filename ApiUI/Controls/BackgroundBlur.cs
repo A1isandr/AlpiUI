1f33cdd872bfa2d579e4620c9b684d3033fe7af2
Alexandr Pidanov <alexandrpi345@gmail.com>
2025-01-18 10:32:50 +0300

    Add BackgroundBlur and BlurCard controls.

---
+++ ApiUI/Controls/BackgroundBlur.cs
@@ -0,0 +1,91 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Platform;
+using Avalonia.Rendering.SceneGraph;
+using Avalonia.Skia;
+using SkiaSharp;
+
+namespace ApiUI.Controls;
+
+public class BackgroundBlur : Control
+{
+    /// <summary>
+    /// Defines the <see cref="BlurRadius"/> property.
+    /// </summary>
+    public static readonly StyledProperty<double> BlurRadiusProperty = 
+        AvaloniaProperty.Register<BackgroundBlur, double>(nameof(BlurRadius), defaultValue: 10.0);
+    
+    /// <summary>
+    /// Background blur radius.
+    /// </summary>
+    public double BlurRadius
+    {
+        get => GetValue(BlurRadiusProperty);
+        set => SetValue(BlurRadiusProperty, value);
+    }
+
+    static BackgroundBlur()
+    {
+        AffectsRender<BackgroundBlur>(BlurRadiusProperty);
+    }
+        
+    public override void Render(DrawingContext context)
+    {
+        context.Custom(new BackdropBlurDrawOperation(Bounds, BlurRadius));
+        
+        base.Render(context);
+    }
+}
+
+
+
+// Made with help from
+// https://github.com/kikipoulet/SukiUI/blob/main/SukiUI/Controls/GlassMorphism/BlurBackground.cs
+internal class BackdropBlurDrawOperation(Rect controlBounds, double blurRadius) : ICustomDrawOperation
+{
+    private readonly Rect _controlBounds = controlBounds;
+    private readonly double _blurRadius = blurRadius;
+
+    public Rect Bounds => _controlBounds;
+    
+    public bool Equals(ICustomDrawOperation? other)
+    {
+        return other is BackdropBlurDrawOperation operation && 
+               operation._controlBounds == _controlBounds &&
+               Math.Abs(operation._blurRadius - _blurRadius) < 1e-6;
+    }
+
+    public void Dispose() { }
+
+    public bool HitTest(Point p) => _controlBounds.Contains(p);
+    
+    public void Render(ImmediateDrawingContext context)
+    {
+        var leaseFeature = context.TryGetFeature<ISkiaSharpApiLeaseFeature>();
+        if (leaseFeature is null) return;
+        
+        using var lease = leaseFeature.Lease();
+        var canvas = lease.SkCanvas;
+
+        if (!canvas.TotalMatrix.TryInvert(out var invertedTransform)) return;
+        
+        using var backgroundSnapshot = lease.SkSurface?.Snapshot();
+        if (backgroundSnapshot is null) return;
+
+        using var backgroundShader = SKShader.CreateImage(
+            backgroundSnapshot,
+            SKShaderTileMode.Clamp,
+            SKShaderTileMode.Clamp,
+            invertedTransform);
+
+        using var blurFilter = SKImageFilter.CreateBlur((float)_blurRadius, (float)_blurRadius);
+
+        using var blurPaint = new SKPaint();
+        blurPaint.Shader = backgroundShader;
+        blurPaint.ImageFilter = blurFilter;
+        blurPaint.IsAntialias = true;
+        
+        canvas.DrawRect(0, 0, (float)_controlBounds.Width, (float)_controlBounds.Height, blurPaint);
+    }
+}
--- ApiUI/Controls/BlurCard.cs
+++ ApiUI/Controls/BlurCard.cs
@@ -1,39 +1,24 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Controls.Primitives;
-using Avalonia.Media;
-using Avalonia.Media.Imaging;
+using Avalonia.Controls.Metadata;
 
 namespace ApiUI.Controls;
 
+[TemplatePart("PART_BackgroundBlur", typeof(BackgroundBlur), IsRequired = true)]
 public class BlurCard : ContentControl
 {
-    public static readonly StyledProperty<double> BlurRadiusProperty = 
-        AvaloniaProperty.Register<BlurCard, double>(nameof(BlurRadius), 10.0);
-    
+    /// <summary>
+    /// Defines the <see cref="BlurRadius"/> property.
+    /// </summary>
+    public static readonly StyledProperty<double> BlurRadiusProperty =
+        AvaloniaProperty.Register<BlurCard, double>(nameof(BlurRadius), defaultValue: 10.0);
+
+    /// <summary>
+    /// Background blur radius.
+    /// </summary>
     public double BlurRadius
     {
         get => GetValue(BlurRadiusProperty);
         set => SetValue(BlurRadiusProperty, value);
-    }
-
-    public static readonly DirectProperty<BlurCard, RenderTargetBitmap?> BlurBitmapProperty =
-        AvaloniaProperty.RegisterDirect<BlurCard, RenderTargetBitmap?>(
-            nameof(BlurBitmap),
-            b => b.BlurBitmap);
-    
-    private RenderTargetBitmap? _blurBitmap;
-
-    public RenderTargetBitmap? BlurBitmap
-    {
-        get => _blurBitmap;
-        private set => SetAndRaise(BlurBitmapProperty, ref _blurBitmap, value);
-    }
-        
-    public override void Render(DrawingContext context)
-    {
-        base.Render(context);
-        
-        BlurBitmap = new RenderTargetBitmap(PixelSize.FromSize(new Size(Bounds.Width, Bounds.Height), 1));
     }
 }
