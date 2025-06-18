cb9208e7194daa08e47755a2c03e88d7a64837a4
Alexandr Pidanov <alexandrpi345@gmail.com>
2025-06-19 01:13:39 +0300

    Add simple demo app.

---
+++ AlpiUI.Demo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Threading.Tasks;
+using DynamicData;
+using ReactiveUI;
+
+namespace AlpiUI.Demo.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    private readonly SourceList<string> __messages = new();
+
+    private readonly ReadOnlyObservableCollection<string> _messages;
+    public ReadOnlyObservableCollection<string> Messages => _messages;
+    
+    public ReactiveCommand<Unit, Unit> Refresh { get; }
+    
+     public MainWindowViewModel()
+     {
+         __messages
+             .Connect()
+             .Bind(out _messages)
+             .Subscribe();
+
+         Refresh = ReactiveCommand.Create(() =>
+         {
+             __messages.Clear();
+             
+             var result = GenerateMessages(); 
+         });
+
+         _ = GenerateMessages();
+     }
+
+    private async Task GenerateMessages()
+    {
+        await Task.Run(async () =>
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                __messages.Add("Message");
+                await Task.Delay(TimeSpan.FromMilliseconds(500));
+            }
+        });
+    }
+}
